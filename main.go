76b6e1041651fa6d317dbd700433b6e1760b6fb3
I766934 <atanas.bozhanin@sap.com>
2025-10-17 21:29:23 +0300

    feat: add iif helper

--- main.go
+++ main.go
@@ -6,3 +6,11 @@ func main() {
 	fmt.Println("Hello, World!")
 	fmt.Println("This is a sample Go application.")
 }
+
+func Iif[T any](condition bool, trueVal, falseVal T) T {
+	if condition {
+		return trueVal
+	}
+
+	return falseVal
+}
